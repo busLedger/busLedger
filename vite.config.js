abd1ee86bc23d0bcf51af520b1909ebcfec12bdb
HaroruDa3 <harol.kock34@gmail.com>
2025-03-03 12:25:46 -0600

    cambios en la ruta

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
+  build: {
+    rollupOptions: {
+      input: resolve(new URL('index.html', import.meta.url).pathname),
+    },
+  },
+  publicDir: 'public',
 })
